bbcafd65a867cea76fbc7039ccdf545832bfc29a
Sonny Cruz <sonnybalut@yahoo.com>
2026-03-26 09:18:02 -0700

    added nrf24l01 receive

---
+++ nrfRx/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "nrf24.h"
+
+int main(){
+  stdio_init_all();
+  sleep_ms(1000);
+  printf("starting...\n");
+  nrf24_init();
+  nrf24_modeRX();
+  char message[7];
+  while(1){
+    if(nrf24_newMessage()){
+      printf("getting new message\n");
+      nrf24_getMessage((uint8_t*)&message);
+      printf("data: %s\n",message);
+      sleep_ms(300);
+    }
+  }
+  return 0;
+}
